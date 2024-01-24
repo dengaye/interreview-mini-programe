0184f31693a6f185f74e27715c879dfd6456d517
dengaye <1277050972@qq.com>
2024-01-24 21:40:57 +0800

    feat: 调整登录页面的样式

--- uni_modules/uni-id-pages/pages/register/register.vue
+++ uni_modules/uni-id-pages/pages/register/register.vue
@@ -1,47 +1,49 @@
 <!-- 账号注册页 -->
 <template>
-	<view class="uni-content">
-		<match-media :min-width="690">
-			<view class="login-logo">
-				<image :src="logo"></image>
-			</view>
-			<!-- 顶部文字 -->
-			<text class="title title-box">用户名密码注册</text>
-		</match-media>
-		<uni-forms ref="form" :value="formData" :rules="rules" validate-trigger="submit" err-show-type="toast">
-			<uni-forms-item name="username" required>
-				<uni-easyinput :inputBorder="false" :focus="focusUsername" @blur="focusUsername = false"
-					class="input-box" placeholder="请输入用户名" v-model="formData.username" trim="both" />
-			</uni-forms-item>
-			<uni-forms-item name="nickname">
-				<uni-easyinput :inputBorder="false" :focus="focusNickname" @blur="focusNickname = false"
-					class="input-box" placeholder="请输入用户昵称" v-model="formData.nickname" trim="both" />
-			</uni-forms-item>
-			<uni-forms-item name="password" v-model="formData.password" required>
-				<uni-easyinput :inputBorder="false" :focus="focusPassword" @blur="focusPassword = false"
-					class="input-box" maxlength="20"
-					:placeholder="'请输入' + (config.passwordStrength == 'weak'?'6':'8') + '-16位密码'" type="password"
-					v-model="formData.password" trim="both" />
-			</uni-forms-item>
-			<uni-forms-item name="password2" v-model="formData.password2" required>
-				<uni-easyinput :inputBorder="false" :focus="focusPassword2" @blur="focusPassword2 =false"
-					class="input-box" placeholder="再次输入密码" maxlength="20" type="password" v-model="formData.password2"
-					trim="both" />
-			</uni-forms-item>
-			<uni-forms-item>
-				<uni-captcha ref="captcha" scene="register" v-model="formData.captcha" />
-			</uni-forms-item>
-			<uni-id-pages-agreements scope="register" ref="agreements"></uni-id-pages-agreements>
-			<button class="uni-btn" type="primary" @click="submit">注册</button>
-			<button @click="navigateBack" class="register-back">返回</button>
+	<ezpage>
+		<view slot="contentSection" class="uni-content">
 			<match-media :min-width="690">
-				<view class="link-box">
-					<text class="link" @click="registerByEmail">邮箱验证码注册</text>
-					<text class="link" @click="toLogin">已有账号？点此登录</text>
+				<view class="login-logo">
+					<image :src="logo"></image>
 				</view>
+				<!-- 顶部文字 -->
+				<text class="title title-box">用户名密码注册</text>
 			</match-media>
-		</uni-forms>
-	</view>
+			<uni-forms ref="form" :value="formData" :rules="rules" validate-trigger="submit" err-show-type="toast">
+				<uni-forms-item name="username" required>
+					<uni-easyinput :inputBorder="false" :focus="focusUsername" @blur="focusUsername = false"
+						class="input-box" placeholder="请输入用户名" v-model="formData.username" trim="both" />
+				</uni-forms-item>
+				<uni-forms-item name="nickname">
+					<uni-easyinput :inputBorder="false" :focus="focusNickname" @blur="focusNickname = false"
+						class="input-box" placeholder="请输入用户昵称" v-model="formData.nickname" trim="both" />
+				</uni-forms-item>
+				<uni-forms-item name="password" v-model="formData.password" required>
+					<uni-easyinput :inputBorder="false" :focus="focusPassword" @blur="focusPassword = false"
+						class="input-box" maxlength="20"
+						:placeholder="'请输入' + (config.passwordStrength == 'weak'?'6':'8') + '-16位密码'" type="password"
+						v-model="formData.password" trim="both" />
+				</uni-forms-item>
+				<uni-forms-item name="password2" v-model="formData.password2" required>
+					<uni-easyinput :inputBorder="false" :focus="focusPassword2" @blur="focusPassword2 =false"
+						class="input-box" placeholder="再次输入密码" maxlength="20" type="password" v-model="formData.password2"
+						trim="both" />
+				</uni-forms-item>
+				<uni-forms-item>
+					<uni-captcha ref="captcha" scene="register" v-model="formData.captcha" />
+				</uni-forms-item>
+				<uni-id-pages-agreements scope="register" ref="agreements"></uni-id-pages-agreements>
+				<button class="uni-btn" type="primary" @click="submit">注册</button>
+				<button @click="navigateBack" class="register-back">返回</button>
+				<match-media :min-width="690">
+					<view class="link-box">
+						<text class="link" @click="registerByEmail">邮箱验证码注册</text>
+						<text class="link" @click="toLogin">已有账号？点此登录</text>
+					</view>
+				</match-media>
+			</uni-forms>
+		</view>
+	</ezpage>
 </template>
 
 <script>
@@ -52,9 +54,13 @@
 		store,
 		mutations
 	} from '@/uni_modules/uni-id-pages/common/store.js'
+	import ezpage from '@/components/ezpage.vue';
 
 	const uniIdCo = uniCloud.importObject("uni-id-co")
 	export default {
+		components: {
+			ezpage,
+		},
 		mixins: [mixin],
 		data() {
 			return {
@@ -146,7 +152,6 @@
 		.uni-content {
 			margin-top: 15px;
 			height: 100%;
-			background-color: #fff;
 		}
 	}
 
